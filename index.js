63ef7b17afc77eaf381c5a351c6d4cf6b4c53786
Rafael Santos <rafaelnsantos1@gmail.com>
2020-01-13 14:55:17 -0300

    multiple templates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const createCmd = require('./lib/create');
 prog
   .version('1.0.0')
   .argument('<folder>', 'Folder to create')
+  .option('--template <template>', 'Template')
   .action(createCmd);
 
 prog.parse(process.argv);
-
