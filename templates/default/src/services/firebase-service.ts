1427132754f808cfa1b53078d3e4b2c50357f435
Rafael Santos <rafaelnsantos1@gmail.com>
2020-01-14 17:31:54 -0300

    easy way to mock verifyToken

--- templates/default/src/services/firebase-service.ts
+++ templates/default/src/services/firebase-service.ts
@@ -1,19 +1,16 @@
 import { FirebaseService } from 'firebase-service-plugin'
 
 export default FirebaseService(() => {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const jwt = require('jsonwebtoken')
   // mock used functions
   let mockAuth: any
   return {
     auth: {
       verifyIdToken: async (token, checkRevoked) => {
-        const { uid, email } = await jwt.verify(token, 'test')
-        const mockRes: any = {
+        const { uid, email } = JSON.parse(token)
+        return {
           uid,
           email
         }
-        return mockRes
       }
     },
     ...mockAuth
